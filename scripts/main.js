07ae31b27510b526e78bf61db56b04195f1ca349
Abhi Khatri <abhikhatri391@gmail.com>
2018-04-17 14:35:04 +0530

    Added Support For Multiple Pickers in single page

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,45 @@
 $(document).ready(function() {
 	
-	var offset = $("#dimension-picker-mousecatcher").offset();
+	$('.dimension-picker-mousecatcher').mousemove(function(e) {
 
-	$('#dimension-picker-mousecatcher').mousemove(function(e) {
+		var currentElement = $(this);
+		var offset = currentElement.offset();
+		var relativeX = (e.pageX - offset.left);
+		var relativeY = (e.pageY - offset.top);
 
-	  var offset = $(this).offset();
-	  var relativeX = (e.pageX - offset.left);
-	  var relativeY = (e.pageY - offset.top);
+		var highlightedCols = Math.ceil(relativeX/18);
+		var highlightedRows = Math.ceil(relativeY/18);
 
-	  var highlightedCols = Math.ceil(relativeX/18);
-	  var highlightedRows = Math.ceil(relativeY/18);
+		var updateCurrentMatrix = function(updateElement, updatedValue){
+			
+			if(updatedValue <= 19 && updatedValue >= 5){
+				updatedValue = updatedValue+1;
+			} else if(updatedValue < 5) {
+				updatedValue = 5;
+			}
 
-	  var updateCurrentMatrix = function(updateElement, updatedValue){
-	  	
-	  	if(updatedValue <= 19 && updatedValue >= 5){
-	  		updatedValue = updatedValue+1;
-		  } else if(updatedValue < 5) {
-		  	updatedValue = 5;
-		  }
+			switch(updateElement){
+				case 'row':
+					currentElement.siblings('.dimension-picker-unhighlighted').css('height', updatedValue + 'em');
+					break;
 
-		  switch(updateElement){
-	  		case 'row':
-	  			$('#dimension-picker-unhighlighted').css('height', updatedValue + 'em');
-	  			break;
+				case 'col':
+					currentElement.siblings('.dimension-picker-unhighlighted').css('width', updatedValue + 'em');	
+					break;
+			}
 
-	  		case 'col':
-	  			$('#dimension-picker-unhighlighted').css('width', updatedValue + 'em');	
-	  			break;
-	  	}
-
-	  };
+		};
 
 	  if(highlightedCols && highlightedRows){
 	  	updateCurrentMatrix('row', highlightedRows);
 	  	updateCurrentMatrix('col', highlightedCols);
-	  	$('#dimension-picker-highlighted').css({'width': highlightedCols + 'em', 'height': highlightedRows + 'em'});
+	  	currentElement.siblings('.dimension-picker-highlighted').css({'width': highlightedCols + 'em', 'height': highlightedRows + 'em'});
 	  } else if(highlightedCols){
 	  	updateCurrentMatrix('col', highlightedCols);
-	  	$('#dimension-picker-highlighted').css('width', highlightedCols + 'em');
+	  	currentElement.siblings('.dimension-picker-highlighted').css('width', highlightedCols + 'em');
 	  } else if(highlightedRows){
 	  	updateCurrentMatrix('row', highlightedRows);
-	  	$('#dimension-picker-highlighted').css('height', highlightedRows + 'em');
+	  	currentElement.siblings('.dimension-picker-highlighted').css('height', highlightedRows + 'em');
 	  }
 		
 
